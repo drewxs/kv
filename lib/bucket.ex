3851322fff3195cce5b251e0f3da1cff7ad6f7be
Andrew X. Shah <drewshah0@gmail.com>
2023-09-10 19:34:56 -0600

    feat(bucket): create delete

--- lib/bucket.ex
+++ lib/bucket.ex
@@ -21,4 +21,11 @@ defmodule KV.Bucket do
   def put(bucket, key, value) do
     Agent.update(bucket, &Map.put(&1, key, value))
   end
+
+  @doc """
+  Deletes a value from the bucket by key.
+  """
+  def delete(bucket, key) do
+    Agent.get_and_update(bucket, &Map.pop(&1, key))
+  end
 end
