c94f4234f4a2f777eebfb8d301911411b3b96c51
Andrew X. Shah <drewshah0@gmail.com>
2023-09-22 00:37:06 -0600

    feat(registry): use ets as cache

--- test/registry_test.exs
+++ test/registry_test.exs
@@ -1,16 +1,16 @@
 defmodule KV.RegistryTest do
   use ExUnit.Case, async: true
 
-  setup do
-    registry = start_supervised!(KV.Registry)
-    %{registry: registry}
+  setup context do
+    _ = start_supervised!({KV.Registry, name: context.test})
+    %{registry: context.test}
   end
 
   test "spawn buckets", %{registry: registry} do
-    assert KV.Registry.lookup(registry, "item") == :error
+    assert KV.Registry.lookup(registry, "reg") == :error
 
-    KV.Registry.create(registry, "item")
-    assert {:ok, bucket} = KV.Registry.lookup(registry, "item")
+    KV.Registry.create(registry, "reg")
+    assert {:ok, bucket} = KV.Registry.lookup(registry, "reg")
 
     KV.Bucket.put(bucket, "item", 1)
     assert KV.Bucket.get(bucket, "item") == 1
@@ -20,13 +20,25 @@ defmodule KV.RegistryTest do
     KV.Registry.create(registry, "item")
     {:ok, bucket} = KV.Registry.lookup(registry, "item")
     Agent.stop(bucket)
+
+    _ = KV.Registry.create(registry, "nil")
     assert KV.Registry.lookup(registry, "item") == :error
   end
 
   test "removes bucket on crash", %{registry: registry} do
-    KV.Registry.create(registry, "shopping")
-    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    KV.Registry.create(registry, "reg")
+    {:ok, bucket} = KV.Registry.lookup(registry, "reg")
     Agent.stop(bucket, :shutdown)
-    assert KV.Registry.lookup(registry, "shopping") == :error
+
+    _ = KV.Registry.create(registry, "nil")
+    assert KV.Registry.lookup(registry, "reg") == :error
+  end
+
+  test "bucket can crash at any time", %{registry: registry} do
+    KV.Registry.create(registry, "reg")
+    {:ok, bucket} = KV.Registry.lookup(registry, "reg")
+    Agent.stop(bucket, :shutdown)
+
+    catch_exit(KV.Bucket.put(bucket, "item", 3))
   end
 end
